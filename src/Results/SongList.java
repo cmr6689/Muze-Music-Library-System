41e26c16e1edcacc041037b2b92ab46967099b4e
rlb8800 <rlb8800@g.rit.edu>
2020-02-21 17:17:20 -0500

    Started the structure for the Results using Strategy Pattern

---
+++ src/Results/SongList.java
@@ -0,0 +1,29 @@
+package Results;
+
+public class SongList implements ResultSorter {
+
+    private void sortSongs() {
+
+    }
+
+    private void sortAquistDate() {
+
+    }
+
+    @Override
+    public void sortAlphabetically() {
+
+    }
+
+    @Override
+    public void sortRating() {
+
+    }
+
+    //Sorting Song Results:
+    //Title
+    //Artist Name
+    //Track Name
+    //Track Duration
+    //Min Rating
+}
