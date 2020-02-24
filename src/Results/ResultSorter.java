d612cfb932ca72b143383d3e9b6000e6a04607b7
fahdm <fahdmasood99@hotmail.com>
2020-02-24 17:19:59 -0500

    Adds all sorting method headers to the ResultSorter Interface

--- src/Results/ResultSorter.java
+++ src/Results/ResultSorter.java
@@ -6,6 +6,12 @@ public interface ResultSorter {
 
     void sortAlphabetically();
     void sortRating();
+    void sortArtist();
+    void sortRelease();
+    void sortAcquistDate();
+    void sortChronologically();
+    void sortSong();
+
 
 
 
