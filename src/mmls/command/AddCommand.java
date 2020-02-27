1013a4c38a880fe6b53e0009659265a874ab30ac
Shane Burke <shanedonburke@gmail.com>
2020-02-27 12:34:02 -0500

    Implement AddSongCommand and AddReleaseCommand

--- src/mmls/command/AddCommand.java
+++ src/mmls/command/AddCommand.java
@@ -4,13 +4,28 @@ import Database.Item;
 import mmls.library.Library;
 import mmls.library.LibraryItem;
 
-public class AddCommand extends LibraryCommand {
-    public AddCommand(Library library, Item targetItem) {
-        super(library, targetItem);
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public abstract class AddCommand implements Command {
+    protected Library library;
+    protected Item newItem;
+    protected Date acquisitionDate;
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+
+    public AddCommand(Library library, Item newItem) {
+        this.library = library;
+        this.newItem = newItem;
+        this.acquisitionDate = new Date();
+    }
+
+    public AddCommand(Library library, Item newItem, String acquisitionDate) throws ParseException {
+        this.library = library;
+        this.newItem = newItem;
+        this.acquisitionDate = dateFormat.parse(acquisitionDate);
     }
 
     @Override
-    public void executeCommand() {
-
-    }
+    public abstract void executeCommand();
 }
