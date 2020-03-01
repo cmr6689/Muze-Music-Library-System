1a5712de0570b6c5a3a0d0d569a7e871be9da4ef
Shane Burke <shanedonburke@gmail.com>
2020-03-01 16:21:54 -0500

    Add sortBy fields to regular expressions for library searches.

--- src/mmls/command/HelpCommand.java
+++ src/mmls/command/HelpCommand.java
@@ -14,10 +14,16 @@ public class HelpCommand implements Command {
         System.out.println("Fields enclosed in brackets [] are optional.");
         System.out.println("Fields enclosed in curly braces {} are required.");
         System.out.println("Fields separated by | accept either one of the enclosed options.");
+        System.out.println("All dates must be formatted \"yyyy-mm-dd\".");
+        System.out.println("For \"sort by\" fields, these are the following options:");
+        System.out.println("  * 1 - Sort alphabetically");
+        System.out.println("  * 2 - Sort by rating (descending)");
+        System.out.println("  * 3 - Sort by acquisition date (ascending)");
+        System.out.println("  * 4 - Sort by release date (ascending) [Note: releases only!]");
         System.out.println("--- Available commands ---");
         System.out.println("database search artist [keywords]");
-        System.out.println("database search song [title] [artist name] [min duration] [max duration] [min rating]");
-        System.out.println("database search release [title] [artist name] [artist GUID] [track name] [track GUID] [min date] [max date]");
+        System.out.println("database search song [-t <title>] [-an <artist name>] [-mind <min. duration>] [-maxd <max. duration>]");
+        System.out.println("database search release [-t <title>] ([-an <artist name>] | [-aid <artist GUID>]) ([-tn <track name>] | [-tid <track GUID>]) [-dr <earliest date> <latest date>]");
         System.out.println("library search artist [name] [type] [min rating]");
         System.out.println("library search song [title] [artist name] [artist GUID] [release title] [release GUID] [min duration] [max duration] [min rating]");
         System.out.println("library search release [title] [artist name] [artist GUID] [track title] [track GUID] [min duration] [max duration] [min rating]");
