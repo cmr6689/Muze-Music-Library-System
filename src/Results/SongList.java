0ecca515aaf91458f9abfa78bf86432020268802
rlb8800 <rlb8800@g.rit.edu>
2020-03-01 15:51:18 -0500

    Changes

--- src/Results/SongList.java
+++ src/Results/SongList.java
@@ -1,11 +1,9 @@
 package Results;
 
-import Database.Database;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class SongList implements ResultSorter {
+    //Test
 
     private void sortSongs() {
 
@@ -15,124 +13,17 @@ public class SongList implements ResultSorter {
     }
 
 
-    private void createSongRatingsList(){
-
-    }
-    private void createSongNameList(){
-
-    }
-
-    /**
-     * Will sort the results alphabetically
-     * Every result will be sorted alphabetically
-     * Uses: sortArtistName(), sortRating(), sortType()
-     */
     @Override
     public void sortAlphabetically(ArrayList<String> updatedList) {
-        java.util.Collections.sort(updatedList);
-        System.out.println(updatedList);
+
     }
 
-    /**
-     * Finds all songs based off the user min rating.
-     * Example: User inputs 4, finds all artists with rating better >= 4
-     * Uses: createSongRatingsList()
-     */
     @Override
     public void sortRating(ArrayList<String> arrayList, int userRating) {
-        createSongRatingsList();
-
-        ArrayList<String> updatedList = new ArrayList<>();
-        int length = arrayList.size();
-        for(int index = 0; index < length; index++) {
-            String songInfo = arrayList.get(index);
-            String[] info = songInfo.split("-");
-            String rate = info[1];
-            int Rating = Integer.getInteger(rate);
-            if(Rating >= userRating) {
-                updatedList.add(info[0]);
-            }
-        }
-        sortAlphabetically(updatedList);
 
     }
 
-    private void sortTitle(ArrayList<String> songs, String words){
-        createSongNameList();
 
-        ArrayList<String> updatedSongs = new ArrayList<>();
-        int length = songs.size();
-        for(int index = 0; index < length; index++) {
-            String song = songs.get(index);
-            if(songs.contains(words)) {
-                updatedSongs.add(song);
-            }
-        }
-        sortAlphabetically(updatedSongs);
-    }
-
-
-    private void createSongMaxMap(){
-
-    }
-
-    private void createSongMinMap(){
-
-    }
-
-    private void sortMax(HashMap<String, Float> durationList, float max){
-        createSongMaxMap();
-
-        ArrayList<String> updatedSongs = new ArrayList<>();
-        for(String song: durationList.keySet()){
-            if (durationList.get(song) < max){
-                updatedSongs.add(song);
-            }
-        }
-        sortAlphabetically(updatedSongs);
-    }
-
-    private void sortByArtistName(HashMap<String, String> artistMap, String artist){
-        createArtistNameMap();
-        ArrayList<String> updatedSongs = new ArrayList<>();
-        for (String song: artistMap.keySet()){
-            if(artistMap.get(song).contains(artist)){
-                updatedSongs.add(song);
-            }
-        }
-        sortAlphabetically(updatedSongs);
-
-    }
-
-    private void sortByReleaseTitle(HashMap<String, String> releaseMap, String release){
-        createReleaseTitleMap();
-        ArrayList<String> updatedSongs = new ArrayList<>();
-        for (String song: releaseMap.keySet()){
-            if(releaseMap.get(song).contains(release)){
-                updatedSongs.add(song);
-            }
-        }
-        sortAlphabetically(updatedSongs);
-
-    }
-
-    private void createArtistNameMap(){
-
-    }
-    private void createReleaseTitleMap(){
-
-    }
-    private void sortMin(HashMap<String, Float> durationMap, float min){
-        createSongMinMap();
-
-        ArrayList<String> updatedSongs = new ArrayList<>();
-        for(String song: durationMap.keySet()){
-            if (durationMap.get(song) > min){
-                updatedSongs.add(song);
-            }
-        }
-        sortAlphabetically(updatedSongs);
-    }
     //Sorting Song Results:
     //Title
     //Artist Name
