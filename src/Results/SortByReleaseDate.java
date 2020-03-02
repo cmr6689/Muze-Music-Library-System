37f32dcaa3bc5616df272d49ad536627623685e0
rlb8800 <rlb8800@g.rit.edu>
2020-03-01 19:44:50 -0500

    Changes to Comparators and Item

---
+++ src/Results/SortByReleaseDate.java
@@ -0,0 +1,18 @@
+package Results;
+
+
+import Database.Item;
+import Database.Song;
+import Database.Release;
+
+import java.util.Comparator;
+
+public class SortByReleaseDate implements Comparator<Item> {
+    //Sort by the release Date
+
+
+    @Override
+    public int compare(Item o1, Item o2) {
+        return (o1.getDate().compareTo(o2.getDate()));
+    }
+}
