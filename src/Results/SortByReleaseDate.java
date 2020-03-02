5362149ae52d05052fb9e08675577c7b14b11a0a
cmr6689 <cmr6689@g.rit.edu>
2020-03-01 22:15:21 -0500

    removed comments

--- src/Results/SortByReleaseDate.java
+++ src/Results/SortByReleaseDate.java
@@ -7,7 +7,7 @@ import Database.Release;
 
 import java.util.Comparator;
 
-/*public class SortByReleaseDate implements Comparator<Item> {
+public class SortByReleaseDate implements Comparator<Item> {
     //Sort by the release Date.
 
 
@@ -15,4 +15,4 @@ import java.util.Comparator;
     public int compare(Item o1, Item o2) {
         return (o1.getReleaseDate().compareTo(o2.getReleaseDate()));
     }
-}*/
+}
